c1ff32bfcc21fc45f27a4f1c50f22bae142361b9
debian109 <debian109@gmail.com>
2016-12-27 18:31:58 +0700

    remove unused

--- app/src/main/java/com/other/curve/BezierCurve.java
+++ app/src/main/java/com/other/curve/BezierCurve.java
@@ -20,8 +20,7 @@ public class BezierCurve extends View {
 
     private final Paint paint;
     private final Paint paintPoint;
-    List<Point> points=new ArrayList<>();
-    boolean isDraw=false;
+    List<Point> points = new ArrayList<>();
 
     public BezierCurve(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -35,8 +34,7 @@ public class BezierCurve extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        points.add(new Point((int)ev.getX(),(int)ev.getY()));
-        isDraw=true;
+        points.add(new Point((int) ev.getX(), (int) ev.getY()));
         return super.onTouchEvent(ev);
     }
 
@@ -44,54 +42,50 @@ public class BezierCurve extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        for (int i = 0; i < points.size() ; i++) {
-            canvas.drawPoint(points.get(i).x,points.get(i).y,paintPoint);
+        for (int i = 0; i < points.size(); i++) {
+            canvas.drawPoint(points.get(i).x, points.get(i).y, paintPoint);
         }
 
-        int N=1000;
+        int N = 1000;
         for (int i = 0; i < N; i++) {
-            float t=i*1.0f/N;
-            float xt=(float) bezierCurveX(points.size()-1,t);
-            float yt=(float) bezierCurveY(points.size()-1,t);
-            canvas.drawPoint(xt,yt,paint);
+            float t = i * 1.0f / N;
+            float xt = (float) bezierCurveX(points.size() - 1, t);
+            float yt = (float) bezierCurveY(points.size() - 1, t);
+            canvas.drawPoint(xt, yt, paint);
         }
         postInvalidate();
 
     }
 
-    public double bezierCurveX(int n,float t)
-    {
-        if(n<=0)
+    public double bezierCurveX(int n, float t) {
+        if (n <= 0)
             return 0;
-        double Bt=0;
+        double Bt = 0;
         for (int i = 0; i <= n; i++) {
-            Bt+=biominal(n,i)*Math.pow(1-t,n-i)*Math.pow(t,i)*points.get(i).x;
+            Bt += biominal(n, i) * Math.pow(1 - t, n - i) * Math.pow(t, i) * points.get(i).x;
         }
         return Bt;
     }
 
-    public double bezierCurveY(int n,float t)
-    {
-        if(n<=0)
+    public double bezierCurveY(int n, float t) {
+        if (n <= 0)
             return 0;
-        double Bt=0;
+        double Bt = 0;
         for (int i = 0; i <= n; i++) {
-            Bt+=biominal(n,i)*Math.pow(1-t,n-i)*Math.pow(t,i)*points.get(i).y;
+            Bt += biominal(n, i) * Math.pow(1 - t, n - i) * Math.pow(t, i) * points.get(i).y;
         }
         return Bt;
     }
 
-    public long factorial(int n)
-    {
-        long result=1;
+    public long factorial(int n) {
+        long result = 1;
         for (int i = n; i > 1; i--) {
-            result*=i;
+            result *= i;
         }
         return result;
     }
 
-    public double biominal(int n,int k)
-    {
-        return 1.0*factorial(n)/(factorial(n-k)*factorial(k));
+    public double biominal(int n, int k) {
+        return 1.0 * factorial(n) / (factorial(n - k) * factorial(k));
     }
 }
